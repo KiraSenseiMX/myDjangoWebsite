cda76161db3c1aac00695c5842d7b30b224c4040
KiraSenseiMX <mail@kirasensei.com>
2022-12-04 19:44:56 -0600

    creating a template to render

--- homeApp/views.py
+++ homeApp/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world! this is my home page :)")
+    return render(request, 'index.html')
