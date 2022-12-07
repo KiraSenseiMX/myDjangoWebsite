4b01b9f8fe934e17250e73207f15171bde2045ea
KiraSenseiMX <mail@kirasensei.com>
2022-12-06 22:28:35 -0600

    testing the datetime lib

--- wewillreturnatApp/views.py
+++ wewillreturnatApp/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 
 def index(request):
+
+    print("---------------------------------------------")
+
+    # testing the datetime lib
+
+    now = datetime.datetime.now()
+    print(now)
+
+    year = now.year
+    month = now.month
+    day = now.day
+    print(year, month, day)
+
+    hour = now.hour
+    minute = now.minute
+    print(hour, minute)
+
+    print("---------------------------------------------")
+
     return render(request, 'wewillreturnatApp/index.html')
