07bf56dab2ff820608fd39d1f24b7fb64ac2ac20
KiraSenseiMX <mail@kirasensei.com>
2024-01-20 22:11:24 -0600

    remove materialboxed

--- marketplaceApp/static/marketplaceApp/scripts/init.js
+++ marketplaceApp/static/marketplaceApp/scripts/init.js
@@ -1,11 +1,34 @@
+/* var carousels = document.getElementsByClassName("carousel");
+var materialboxes = document.getElementsByClassName("materialboxed");
+
+function hidefunction(){
+  for (var i = 0; i < carousels.length; i++) {
+    carousels.item(i).style.opacity = "0.0";
+  }
+   for (var i = 0; i < materialboxes.length; i++) {
+    materialboxes.item(i).style.opacity = "1.0";
+  }
+}
+
+function showfunction() {
+  for (var i = 0; i < carousels.length; i++) {
+    carousels.item(i).style.opacity = "1.0";
+  }
+} */
+
 (function($){
   $(function(){
 
     $(document).ready(function(){
       $('.carousel').carousel({
-        dist: 0
+        duration: 100,
+        dist: -50,
+        spacing: 2,
       });
-      $('.materialboxed').materialbox();
+/*       $('.materialboxed').materialbox({
+        onOpenEnd: hidefunction,
+        onCloseEnd: showfunction,
+      }); */
     });
 
   }); // end of document ready
